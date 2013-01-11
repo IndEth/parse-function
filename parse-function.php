b8b0bb3b5d1d905920cf892481a3079fdb69e324
urmaul <bogdan@itechcraft.com>
2013-01-11 16:32:55 +0200

    Spaces

--- parse-function.php
+++ parse-function.php
@@ -3,14 +3,14 @@
     {
         $parseContent = array();
         $content = explode('#', $content);
-        foreach ($content as $exp){
+        foreach ($content as $exp) {
             $str = explode("\n", $exp, 2);
             $keyStr = explode('=', $str[0], 2);
-            if($keyStr[0]!=$str[0]){
+            if ($keyStr[0]!=$str[0]) {
                 $key = $keyStr[0];
                 $des = str_replace($keyStr[0].'=', '', $str[0]);
-            }
-            else{
+                
+            } else {
                 $key = $str[0];
                 $des = str_replace($str[0], '', $exp);
             }
