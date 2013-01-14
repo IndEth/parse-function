dcab766cc7333d53b932c74a890436870b731b69
IndEth <iegupov@techs.com.ua>
2013-01-14 12:16:59 +0200

    14.01

--- parse-function.php
+++ parse-function.php
@@ -2,7 +2,7 @@
 	public function parseContent($content)
     {
         $parseContent = array();
-        $expContent = preg_split("/[\n]#+/s", $content);
+        $expContent = explode("\n#", $content);
         if($content{0}!='#')
             unset ($expContent[0]);
         else 
@@ -10,7 +10,7 @@
         foreach ($expContent as $exp){
             $str = explode("\n", $exp, 2);
             $keyStr = explode('=', $str[0], 2);
-            if($keyStr[0]!=$str[0]){
+            if(isset($keyStr[1])){
                 $key = $keyStr[0];
                 $des = $keyStr[1];
             }
@@ -18,8 +18,8 @@
                 $key = $str[0];
                 $des = end($str);
             }
-            trim($key);
-            trim($des);
+            $key = trim($key);
+            $des = trim($des);
             $parseContent[$key] = $des;
         }
         return $parseContent;
